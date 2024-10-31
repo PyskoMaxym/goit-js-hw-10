f36f6797d5125fcbb8bafa3e36126d4cb0567ef5
PyskoMaxym <pisko.maks@gmail.com>
2024-10-31 15:35:48 +0200

    finish

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,10 @@
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
 
 
 const startButton = document.querySelector("[data-start]");
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,3 +1,7 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
 
 document.querySelector(".form").addEventListener("submit", (event) =>{
     event.preventDefault();
