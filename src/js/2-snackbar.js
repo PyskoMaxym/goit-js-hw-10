63db9c24e13d6cf46c69039c6a187adc4160041f
PyskoMaxym <pisko.maks@gmail.com>
2024-10-31 15:13:07 +0200

    task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,33 @@
+
+document.querySelector(".form").addEventListener("submit", (event) =>{
+    event.preventDefault();
+
+    const delay = parseInt(event.target.elements.delay.value);
+    const state = event.target.elements.state.value;
+    
+    const promise = new Promise((resolve, reject) => {
+
+        setTimeout( () => {
+            if(state === "fulfilled"){
+                resolve(delay);
+            } else{
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise
+    .then((delay)=>{
+        iziToast.success({
+            title: "✅ Success",
+            message: `Fulfilled promise in ${delay}ms`,
+        });
+
+    })
+    .catch((delay)=> {
+        iziToast.error({
+            title: "❌ Error",
+            message: `Rejected promise in ${delay}ms`,
+        });
+    });
+}  );
